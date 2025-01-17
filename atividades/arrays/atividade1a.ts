a7d957149a4fbc9126deaae632d960b1a7144816
Jefferson <jeffersonbatistademendonca2000@gmail.com>
2025-01-17 09:04:14 -0300

    feat: Atividades de arrays

---
+++ atividades/arrays/atividade1a.ts
@@ -0,0 +1,13 @@
+const arrayNumeros = [3, 5, 7, 3, 8, 9, 1];
+
+export function quadradosFor(array: number[]): number[] {
+    const resultado: number[] = [];
+    for (let i = 0; i < array.length; i++) {
+        resultado.push(array[i] ** 2);
+    }
+    return resultado;
+}
+
+//resultados 
+const quadrados = quadradosFor(arrayNumeros);
+console.log("Quadrados (usando for):", quadrados);
